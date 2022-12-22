91fbaa66ee263272ddfcb89b6789dcb7beeaae7a
Marvin Chew <mchew@black.ai>
2022-12-23 10:54:01 +1100

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { Box, Heading } from "@chakra-ui/react";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<div className="App">
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src="/vite.svg" className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://reactjs.org" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</div>
+		<Box
+			bg="black"
+			color="white"
+			display="flex"
+			justifyContent="center"
+			alignItems="center"
+			minH="100vh"
+		>
+			<Heading>
+				Time is{" "}
+				<Heading display="inline" color="lime">
+					Money
+				</Heading>
+			</Heading>
+		</Box>
 	);
 }
 
