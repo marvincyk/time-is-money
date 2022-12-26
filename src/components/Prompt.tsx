c02ee24ed0d6240644c0fb7678c9db852ad742c0
Marvin Chew <marvin@marvincyk.com>
2022-12-26 13:42:30 +1100

    MVP

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -1,33 +1,42 @@
-import { useState } from "react";
-import { FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {
+	FormControl,
+	FormLabel,
+	Input,
+	InputGroup,
+	InputLeftElement,
+} from "@chakra-ui/react";
 
 interface PromptProps {
 	onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+	onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+	salary: number;
 }
 
-const Prompt = ({ onSubmit: handleSubmit }: PromptProps) => {
-	const [salary, setSalary] = useState<number>();
-
-	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setSalary(parseInt(event.target.value));
-	};
-
+const Prompt = ({
+	onSubmit: handleSubmit,
+	onChange: handleChange,
+	salary,
+}: PromptProps) => {
 	return (
 		<form onSubmit={handleSubmit}>
 			<FormControl display="flex" flexDir="column" alignItems="center">
 				<FormLabel fontSize="6xl" whiteSpace="nowrap">
 					What is your salary?
 				</FormLabel>
-				<Input
-					type="number"
-					variant="flushed"
-					value={salary}
-					onChange={handleChange}
-					textAlign="center"
-					fontSize="3xl"
-					focusBorderColor="white"
-					isRequired
-				/>
+				<InputGroup>
+					<InputLeftElement pointerEvents="none" children="$" fontSize="3xl" />
+					<Input
+						type="number"
+						variant="flushed"
+						value={salary > 0 ? salary : ""}
+						onChange={handleChange}
+						textAlign="center"
+						fontSize="3xl"
+						focusBorderColor="white"
+						isRequired
+						pr={10}
+					/>
+				</InputGroup>
 			</FormControl>
 		</form>
 	);
